02eb1ac8c046258462217ff19c424551e4d13624
Simon Dudley <simon.dudley@consensys.net>
2023-01-13 15:10:36 +1000

    withdrawals-devnet-1 config

--- startTeku.sh
+++ startTeku.sh
@@ -22,23 +22,35 @@ export GENESIS_TIME=$(($(date +%s) + 120)) # 120s until genesis, feel free to in
 SHANGHAI=$(($GENESIS_TIME + 0))
 # sed -i'' -e "s/\"shanghaiTime\": .*,/\"shanghaiTime\": $SHANGHAI,/" execution-genesis.json
 
+## devops@withdrawals-teku-besu-1
 $TEKU \
-  --ee-endpoint http://127.0.0.1:8551 \
-  --ee-jwt-secret-file="jwtsecret.txt" \
-  --validators-proposer-default-fee-recipient=0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b \
-  --Xinterop-enabled=true \
-  --Xinterop-number-of-validators=256 \
-  --Xinterop-owned-validator-start-index=0 \
-  --Xinterop-owned-validator-count=256 \
-  --Xinterop-genesis-payload-header=genesis-header.json \
-  --network=config.yaml \
-  --p2p-private-key-file=teku.key \
-  --rest-api-enabled \
-  --rest-api-docs-enabled \
-  --Xstartup-target-peer-count=0 \
-  --Xstartup-timeout-seconds=0 \
-  --data-path /tmp/teku
-
-
-
-#  --initial-state "${GENESIS}" \
+ --network=config.yaml \
+ --initial-state=genesis.ssz \
+ --Xee-version=kilnv2 \
+ --data-path=/tmp/teku \
+ --data-storage-mode=PRUNE \
+ --p2p-enabled=true \
+# --p2p-advertised-ip=142.93.108.142 \
+ --p2p-port=9000 \
+ --p2p-advertised-port=9000 \
+ --logging=info \
+ --Xnetwork-total-terminal-difficulty-override=9 \
+ --p2p-peer-upper-bound=100 \
+ --ee-endpoint=http://127.0.0.1:8560 \
+ --p2p-discovery-bootnodes=enr:-LK4QNRkwBq94pldu1dSD59kabjcPBirmHVK_ZxKz-v4JhLeOIWQCJUU5_mTaaaWeP2DHLaHa20ryCEgFgU8V0NusOgGh2F0dG5ldHOIAAAAAAAAAACEZXRoMpB39OJhIAAAOQEAAAAAAAAAgmlkgnY0gmlwhJ9BcfuJc2VjcDI1NmsxoQORzaiwV-ogDNaDpB0S2ptorLxgiyGIw39bH1QCO16iu4N0Y3CCIyiDdWRwgiMo \
+ --p2p-static-peers=/ip4/159.65.113.251/tcp/9000/p2p/16Uiu2HAmNU9MQSjFBPZHLMsmpGP9xnJ16mkHsm1x8jUy6q9vDTsG \
+ --metrics-enabled=true \
+ --metrics-interface=0.0.0.0 \
+ --metrics-port=8000 \
+ --rest-api-enabled=true \
+ --rest-api-docs-enabled=true \
+ --rest-api-interface=0.0.0.0 \
+ --rest-api-port=4000 \
+ --metrics-host-allowlist=* \
+ --log-destination=console \
+ --rest-api-host-allowlist=* \
+ --data-storage-non-canonical-blocks-enabled=true \
+ --ee-jwt-secret-file="jwtsecret.txt" \
+ --Xlog-include-p2p-warnings-enabled \
+ --Xmetrics-block-timing-tracking-enabled \
+ --Xprogressive-balances-mode=USED
