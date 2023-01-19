21421cd93b4a265bf096de51cb59775e116ccfc5
Simon Dudley <simon.dudley@consensys.net>
2023-01-19 19:45:00 +1000

    Compatibility with Marius' tx-fuzz
     - https://github.com/MariusVanDerWijden/tx-fuzz
     - add tx sender account to genesis
     - update genesis hashes
    
    Also enable debug JVM opts

--- startBesu.sh
+++ startBesu.sh
@@ -9,5 +9,7 @@ fi
 
 #export LOG4J_CONFIGURATION_FILE=./log4j2-besu.xml 
 
+export BESU_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
+
 rm -rf /tmp/besu
 $BESU --config-file besu-config.toml $@
