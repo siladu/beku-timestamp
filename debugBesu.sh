de7b9990558f8c26d6967895e9fde5f5fb8463ec
Simon Dudley <simon.dudley@consensys.net>
2023-02-04 09:33:25 +1000

    Add recreation files

--- debugBesu.sh
+++ debugBesu.sh
@@ -8,9 +8,34 @@ curl --retry-delay 50 --retry-connrefused --location \
     "jsonrpc": "2.0",
     "method": "admin_changeLogLevel",
     "params": [
-        "DEBUG", ["org.hyperledger.besu.ethereum.api.jsonrpc"]
+        "DEBUG", ["org.hyperledger.besu"]
     ],
     "id": 1
-}
-'
+}'
 
+curl --retry-delay 50 --retry-connrefused --location \
+     --request POST 'http://127.0.0.1:8545' \
+     --header 'Content-Type: application/json' \
+     --data-raw '{
+    "jsonrpc": "2.0",
+    "method": "admin_changeLogLevel",
+    "params": [
+        "INFO", [
+          "org.hyperledger.besu.ethereum.eth.sync.backwardsync",
+          "org.hyperledger.besu.ethereum.p2p",
+          "org.hyperledger.besu.ethereum.eth.manager.EthProtocolManager",
+          "org.hyperledger.besu.ethereum.eth.manager.EthPeer",
+          "org.hyperledger.besu.ethereum.eth.manager.EthPeers",
+          "org.hyperledger.besu.ethereum.eth.manager.task.RetryingGetHeadersEndingAtFromPeerByHashTask",
+          "org.hyperledger.besu.ethereum.eth.sync.backwardsync.BackwardChain",
+          "org.hyperledger.besu.ethereum.eth.sync.backwardsync.ProcessKnownAncestorsStep",
+          "org.hyperledger.besu.ethereum.eth.transactions",
+          "org.hyperledger.besu.ethereum.blockcreation.BlockTransactionSelector",
+          "org.hyperledger.besu.consensus.merge.PostMergeContext",
+          "org.hyperledger.besu.consensus.merge.UnverifiedForkchoiceSupplier",
+          "org.hyperledger.besu.consensus.merge.TransitionProtocolSchedule",
+          "org.hyperledger.besu.ethereum.bonsai"
+        ]
+    ],
+    "id": 1
+}'
